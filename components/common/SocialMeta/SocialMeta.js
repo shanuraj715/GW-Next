0e86bb3e941e549a742a492444ae974a5e14a47f
Shanu Raj <shanuraj715@gmail.com>
2022-07-28 12:47:38 +0530

    created main layout of the app

---
+++ components/common/SocialMeta/SocialMeta.js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+function SocialMeta(props) {
+  const {
+    data: { url = '', title = '', image = '', description = '', pageIndex = 'index', linkFollow = "follow" },
+  } = props;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="robots" content={`${pageIndex},${linkFollow}`} />
+      <meta property="og:title" content={title} key="facebookTitle" />
+
+      <meta
+        name="twitter:card"
+        content="summary_large_image"
+        key="twitterCard"
+      />
+
+      <meta name="twitter:title" content={title} key="twitterTitle" />
+
+      <meta property="og:type" content="article" key="facebookType" />
+
+      {
+    url && (
+      <>
+        <meta property="og:url" content={url} key="facebookUrl" />
+        <meta name="twitter:url" content={url} key="twitterUrl" />
+      </>
+    )
+  }
+
+  {
+    image && (
+      <>
+        <meta name="thumbnail" content={image} key="googleImage" />
+        <meta property="og:image" content={image} key="facebookImage" />
+        <meta name="twitter:image" content={image} key="twitterImage" />
+      </>
+    )
+  }
+  {
+    description && (
+      <>
+        <meta
+          property="og:description"
+          content={description}
+          key="facebookDescription"
+        />
+        <meta name="description" content={description} key="description" />
+        <meta
+          name="twitter:description"
+          content={description}
+          key="twitterDescription"
+        />
+      </>
+    )
+  }
+    </Head >
+  );
+}
+
+SocialMeta.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default SocialMeta;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
+import '../styles/globals.scss'
+import MainLayout from '../components/MainLayout/MainLayout'
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  })
+
+  return <>
+    <Head>
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  </>
 }
 
 export default MyApp
